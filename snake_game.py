bb2a3c59ab7955d1337e1f850067239a51192031
rahal1231 <thanurikahawita@gmail.com>
2026-06-24 16:37:21 +0200

    Add files via upload

--- snake_game.py
+++ snake_game.py
@@ -6,11 +6,14 @@ snake=Actor("snake")
 apple=Actor("apple")
 snake.pos=100,100
 apple.pos=500,500
+score=0
 def draw():
     screen.fill("green")
     snake.draw()
     apple.draw()
+    screen.draw.text(str(score),(10,10))
 def update():
+    global score
     if keyboard.a:
           snake.x-=5
     if keyboard.d:
@@ -19,6 +22,13 @@ def update():
          snake.y-=5
     if keyboard.s:
          snake.y+=5
+    if snake.colliderect(apple):
+         apple.x=random.randint(80,520)
+         apple.y=random.randint(80,520)
+         score+=1
+
+
+         
          
 
 
